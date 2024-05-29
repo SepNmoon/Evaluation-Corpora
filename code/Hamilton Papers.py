e8681deb9cd273af354afe2736de692a042affc0
SepNmoon <shakaluliu@gmail.com>
2024-05-29 17:33:40 +0100

    fix MH choice issue

--- code/Hamilton Papers.py
+++ code/Hamilton Papers.py
@@ -23,44 +23,87 @@ def iterate_place_person(node):
     
     for child in node:
         if child.tag=='{http://www.tei-c.org/ns/1.0}persName':
-            
-            #将整个pers的字符全转为str            
             pers_str = ET.tostring(child, encoding='unicode')
-            #检查str中是否含lb
-            lb_str=r'<ns0:lb.*?>'
-            result=re.search(lb_str,pers_str,flags=re.DOTALL)
-            if result!=None:
-                #如果有lb，则用改进过的名字
-                new_str1=re.sub(r'<ns0:orig>.*?</ns0:orig>','',pers_str,flags=re.DOTALL)
+            #检查是否有sic
+            sic_str=r'<ns0:sic.*?>'
+            sic_result=re.search(sic_str,pers_str,flags=re.DOTALL)
+            if sic_result!=None:
+                #如果有sic，则去掉更正过的标注，保留错误de
+                name_str=re.sub(r'<ns0:corr>.*?</ns0:corr>','',pers_str,flags=re.DOTALL) 
             else:
-                #如果不含lb，则用原本的名字，即把expan或reg的删掉
-                new_str=re.sub(r'<ns0:expan>.*?</ns0:expan>','',pers_str) 
-                new_str1=re.sub(r'<ns0:reg>.*?</ns0:reg>','',new_str)
+                name_str=pers_str
+                
+            expan_str=r'<ns0:expan.*?>'            
+            expan_result=re.search(expan_str,name_str,flags=re.DOTALL)
+            if expan_result!=None:
+                #如果有扩展的，则去掉扩展，保留原来的缩写
+                name_str=re.sub(r'<ns0:expan>.*?</ns0:expan>','',name_str,flags=re.DOTALL)
+            else:
+                name_str=name_str
+                            
+            lb_str=r'<ns0:lb.*?>'
+            lb_result=re.search(lb_str,name_str,flags=re.DOTALL)
+            if lb_result!=None:
+                #如果有lb，则用改进过的名字 
+                name_str=re.sub(r'<ns0:orig>.*?</ns0:orig>','',name_str,flags=re.DOTALL)
+                              
+            else:
+                #如果不含lb，则用原本的名字，即把reg的删掉
+                name_str=re.sub(r'<ns0:reg>.*?</ns0:reg>','',name_str,flags=re.DOTALL)
+            
+            #如果有大于一个的空格，即出现了lb情况，则将其替代成一个空格
+            space_pattern=r'\s{1,}'
+            name_str=re.sub(space_pattern,' ',name_str)
+            
             
             #接下来针对被嵌套tag分离的str，将他们连接起来
             persName=''
             pattern=r'>(.*?)<'
-            person_result=re.findall(pattern,new_str1)
+            
+            person_result=re.findall(pattern,name_str)
+            
             for i in person_result:  #将被tag分离的名字加起来
                 persName=persName+i 
             all_persName.append(persName)
-            
-            
+                            
         elif child.tag=='{http://www.tei-c.org/ns/1.0}placeName':              
-            xml_str = ET.tostring(child, encoding='unicode')
+            place_str = ET.tostring(child, encoding='unicode')
+            #检查是否有sic
+            sic_str=r'<ns0:sic.*?>'
+            sic_result=re.search(sic_str,place_str,flags=re.DOTALL)
+            if sic_result!=None:
+                #如果有sic，则去掉更正过的标注，保留错误de
+                name_str=re.sub(r'<ns0:corr>.*?</ns0:corr>','',place_str,flags=re.DOTALL) 
+            else:
+                name_str=place_str
+                
+            expan_str=r'<ns0:expan.*?>'            
+            expan_result=re.search(expan_str,name_str,flags=re.DOTALL)
+            if expan_result!=None:
+                #如果有扩展的，则去掉扩展，保留原来的缩写
+                name_str=re.sub(r'<ns0:expan>.*?</ns0:expan>','',name_str,flags=re.DOTALL)
+            else:
+                name_str=name_str
+                            
             lb_str=r'<ns0:lb.*?>'
-            result=re.search(lb_str,xml_str,flags=re.DOTALL)
-            if result!=None:
-                xml_str=re.sub(r'<ns0:orig>.*?</ns0:orig>','',xml_str,flags=re.DOTALL)
-            else:            
-                xml_str=re.sub(r'<ns0:expan>.*?</ns0:expan>','',xml_str) #把expan中的内容删掉
-                xml_str=re.sub(r'<ns0:reg>.*?</ns0:reg>','',xml_str)
-            placeName=''
-            pattern=r'>(.*?)<' #匹配><之间的东西
-            place_result=re.findall(pattern,xml_str)
+            lb_result=re.search(lb_str,name_str,flags=re.DOTALL)
+            if lb_result!=None:
+                #如果有lb，则用改进过的名字
+                name_str=re.sub(r'<ns0:orig>.*?</ns0:orig>','',name_str,flags=re.DOTALL)
+            else:
+                #如果不含lb，则用原本的名字，即把reg的删掉
+                name_str=re.sub(r'<ns0:reg>.*?</ns0:reg>','',name_str,flags=re.DOTALL)
             
-
-            #print(person_result)
+            
+            #如果有大于一个的空格，即出现了lb情况，则将其替代成一个空格
+            space_pattern=r'\s{1,}'
+            name_str=re.sub(space_pattern,' ',name_str)
+            
+            
+            #接下来针对被嵌套tag分离的str，将他们连接起来
+            placeName=''
+            pattern=r'>(.*?)<'
+            place_result=re.findall(pattern,name_str)
             for i in place_result:  #将被tag分离的名字加起来
                 placeName=placeName+i 
             all_placeName.append(placeName)
@@ -81,20 +124,35 @@ def transcribe(node):
 def match(node):
 #将文本与实体类型对应起来
     text_str=ET.tostring(node, encoding='unicode')
-    #print(text_str)
     
-    #遇到choice,如果有lb保留新的，如果没有保留origin
+    
+    #遇到choice,判断sic,org和abbr，之所以要将choice拉出来，因为choice里面会将单词分割
     choice_pattern=r'<ns0:choice.*?</ns0:choice>'
     choice_matches=re.findall(choice_pattern, text_str,flags=re.S)
-    #print(choice_result)
-    lb_pattern=r'<ns0:lb.*?>'
+    
+    sic_str=r'<ns0:sic.*?>'
+    expan_str=r'<ns0:expan.*?>'
+    lb_str=r'<ns0:lb.*?>'
     choice_replace_result=[]
     for all_choice in choice_matches:
-        if re.search(lb_pattern, all_choice,flags=re.S)!=None:
+        if re.search(sic_str, all_choice,flags=re.S)!=None:
+            all_choice=re.sub(r'<ns0:corr>.*?</ns0:corr>','',all_choice,flags=re.S)
+        else:
+            all_choice=all_choice
+        
+        if re.search(expan_str, all_choice,flags=re.S)!=None:
+            all_choice=re.sub(r'<ns0:expan>.*?</ns0:expan>','',all_choice,flags=re.S)
+        else:
+            all_choice=all_choice
+        
+        if re.search(lb_str, all_choice,flags=re.S)!=None:
             all_choice=re.sub(r'<ns0:orig>.*?</ns0:orig>','',all_choice,flags=re.S)
         else:
-            all_choice=re.sub(r'<ns0:expan>.*?</ns0:expan>','',all_choice,flags=re.S)
             all_choice=re.sub(r'<ns0:reg>.*?</ns0:reg>','',all_choice,flags=re.S)
+        #如果有大于一个的空格，即出现了lb情况，则将其替代成一个空格
+        space_pattern=r'\s{1,}'
+        all_choice=re.sub(space_pattern,' ',all_choice)
+        
         #接下来将choice里所有被tag阻断的字符连接起来
         pattern=r'>(.*?)<' #匹配><之间的东西
         result=re.findall(pattern,all_choice)
@@ -104,7 +162,6 @@ def match(node):
             new_result=new_result+letter
         #print(new_result)
         choice_replace_result.append(new_result)
-    #print(choice_replace_result)
     #开始替换全文中的choice
     choice_num=len(choice_replace_result)
     choice_replacements=[]
@@ -115,6 +172,8 @@ def match(node):
     for match,replacement in zip(choice_matches, choice_replacements):
         text_str=text_str.replace(match,replacement,1)
     
+        
+   
     #接下来是将所有人名tag换成all_persName
     pers_pattern=r'<ns0:persName.*?</ns0:persName>'
     pers_matches=re.findall(pers_pattern,text_str,flags=re.S)
@@ -139,6 +198,7 @@ def match(node):
     text_str=text_str.replace('&amp;','&')
     group_pattern=r'>(.*?)<' #匹配><之间的东西
     group_result=re.findall(group_pattern,text_str,flags=re.S)
+    
     #print(match_result)
     new_group=[]
     #去空值，只把有文本的留下
@@ -228,7 +288,7 @@ def write_csv(file):
     print(len(all_tokens),len(all_entity))
     file=file.replace('.xml','.tsv')
     dataframe = pd.DataFrame({'TOKEN':all_tokens,'NE':all_entity})
-    dataframe.to_csv('D:\OneDrive - University College London\Desktop\Corpora\code\MH\%s'%file,encoding="utf_8_sig",index=False)
+    dataframe.to_csv('D:\OneDrive - University College London\Desktop\check\%s'%file,encoding="utf_8_sig",index=False)
     
     
 
@@ -236,7 +296,7 @@ if __name__ == "__main__":
     path='D:\OneDrive - University College London\Desktop\Corpora\Mary Hamilton papers\XML_files_minus_project-specific_mark-up_20240212'   
     files= os.listdir(path)
     
-    file='AR-HAM-00001-00008-00002-00007.xml'
+    file='AR-HAM-00001-00004-00002-00016.xml'
     all_node=[]
     all_persName=[]
     all_placeName=[]
@@ -244,15 +304,19 @@ if __name__ == "__main__":
     iterate_place_person(text_node)
     print(all_persName)
     print(all_placeName)
-    #for file in files[0:1598]:  
-        #all_node=[]
-        #all_persName=[]
-        #all_placeName=[]
-        #text=''
-        #text_node=read_file(file)
-        #iterate_place_person(text_node)
+    #write_csv(file)
+    
+    '''
+    for file in files[0:1598]:  
+        all_node=[]
+        all_persName=[]
+        all_placeName=[]
+        text=''
+        text_node=read_file(file)
+        iterate_place_person(text_node)
         #transcribe(text_node)
-        #write_csv(file)
+        write_csv(file)
+    '''
         
    
     
